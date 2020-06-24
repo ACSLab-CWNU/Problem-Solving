d8bc77fa7567d6d55e78dff107b755e2f1fac307
Fata11 <50870257+Fata11@users.noreply.github.com>
2020-06-24 13:39:08 +0900

    Add files via upload

---
+++ 실4)9012번-괄호.py
@@ -0,0 +1,17 @@
+N = int(input())
+for i in range(N):
+    string = input()
+    s = list(string)
+    sum = 0
+    for i in s:
+        if i == '(':
+            sum += 1
+        elif i == ')':
+            sum -= 1
+        if sum < 0:
+            print('NO')
+            break
+    if sum > 0:
+        print('NO')
+    elif sum == 0:
+        print('YES')
